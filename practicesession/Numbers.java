c64dbe0aaf70da9aaaf4604f0659068b4cb8ef27
Sohail <168761154+Zohell@users.noreply.github.com>
2026-07-01 18:07:48 +0530

    Add files via upload

---
+++ practicesession/Numbers.java
@@ -0,0 +1,15 @@
+package practicesession;
+
+public class Numbers {
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i++) {
+			System.out.println(i);
+		}
+	}
+//	1
+//	2
+//	3
+//	4
+//	5
+
+}
---
+++ practicesession/RightTrangle.java
@@ -0,0 +1,18 @@
+package practicesession;
+
+public class RightTrangle {
+public static void main(String[] args) {
+	
+	for(int i=1;i<=5;i++) {
+		for(int j=1;j<=i;j++) {
+			System.out.print("*");
+			
+		}System.out.println();
+		}
+//	*
+//	**
+//	***
+//	****
+//	*****
+}
+}
